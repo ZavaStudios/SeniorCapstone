9abcdded59c2a031ea8a65e7e8e81dd2f3c188e9
unknown <evens@TEAMUNITY.users.coe.utah.edu>
2014-01-21 18:09:14 -0700

    Created a function to allow for a random group of enemies to be placed within a room. Haven't tested this yet, but will do so either later tonight or tomorrow afternoon.

---
+++ Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+
+public class GenerateEnemies
+{
+	ArrayList enemyList = new ArrayList();
+	Random rand = new Random();
+
+	public enum enemy
+	{
+		spider, skeleton, zombie
+	}
+
+	//Generates enemies at random based on the number of points that are allocated to that room.
+	// Returns an ArrayList of type enemy with the enemies that should be contained in any given room.
+	public ArrayList generateEnemies(int points)
+	{
+		//Fuck you Victor. Here's a comment for you! :) PS Angel Beats is the best! :DDDD 
+		int mod = points%5;
+		int dif;
+
+		//Check to see if we have an exact number of points.
+		if(mod != 0)
+		{
+			//Probability to round up or down is modified based on how close it is to 5.
+			dif = 5 - mod;
+
+			//50% chance to fill the remaining points with spiders.
+			if(rand.Next(2) == 0)
+			{
+				//Make it even point values and add spiders in. 
+				for(; points%5 == 0; points--)
+					enemyList.Add(enemy.spider);
+			}
+			else
+			{
+				//Round up if we get a value greater than our threshold.
+				if(dif < rand.Next(5))
+				{
+					points += dif;
+				}
+				//Round down if below threshold.
+				else
+				{
+					points -= mod;
+				}
+			}
+
+			//Fill in the remaining points with random enemies.
+			for(int i = 0; i < points; i += 5)
+			{
+				switch(rand.Next(3))
+				{
+					case 0: 
+					{	
+						enemyList.Add(enemy.skeleton);
+						break;
+					}
+					case 1:
+					{
+						//Add 5 spiders. :)
+						enemyList.Add (enemy.spider);
+						enemyList.Add (enemy.spider);
+						enemyList.Add (enemy.spider);
+						enemyList.Add (enemy.spider);
+						enemyList.Add (enemy.spider);
+						break;
+					}
+					case 2:
+					{
+						enemyList.Add(enemy.zombie);
+						break;
+					}	
+				}
+			}
+		}
+
+		return enemyList;
+	}
+}
