4d4eacf0f3be0b5c9e45e52d15c140c24c8c0adb
unknown <aric.parkinson@gmail.com>
2014-04-14 23:39:48 -0600

    Added item texture switcher, and attached to relevent prefabs.

---
+++ Assets/Scripts/Items/PlayerStaffTextureSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStaffTextureSwitcher : ItemTextureSwitcher
+{
+    public override void SwitchTexture(ItemBase.tOreType type)
+    {
+        switch (type)
+        {
+            case ItemBase.tOreType.Bone:
+                transform.FindChild("Model").renderer.material = materials[0];
+                break;
+            case ItemBase.tOreType.Copper:
+                transform.FindChild("Model").renderer.material = materials[1];
+                break;
+            case ItemBase.tOreType.Dragon:
+                transform.FindChild("Model").renderer.material = materials[2];
+                break;
+            case ItemBase.tOreType.Ethereal:
+                transform.FindChild("Model").renderer.material = materials[3];
+                break;
+            case ItemBase.tOreType.Iron:
+                transform.FindChild("Model").renderer.material = materials[4];
+                break;
+            case ItemBase.tOreType.Mithril:
+                transform.FindChild("Model").renderer.material = materials[5];
+                break;
+            case ItemBase.tOreType.Steel:
+            default:
+                transform.FindChild("Model").renderer.material = materials[6];
+                break;
+        }
+    }
+}
